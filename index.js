075875e420c266eed7ea1ce4e7d910c431ef0118
Daloar Hossan <daloarhossan128@gmail.com>
2022-05-23 23:05:27 +0600

    products-get

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ app.use(express.json())
 app.get('/',(req,res)=>{res.send('Running')})
 
 
-
 const uri =` mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.j7629.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 const run=async()=>{
